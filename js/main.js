7d63b18226219eabead57f7d34239d54045063fc
Luis Laurente <137670663+LuisLaurente@users.noreply.github.com>
2025-04-20 13:34:58 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Archivo JavaScript principal
-document.addEventListener('DOMContentLoaded', function() {
-    // Código común para todas las páginas
-    console.log('Fab Lab iFurniture - Sitio web cargado');
-    
-    // Aquí puedes añadir cualquier otra funcionalidad general
-});
